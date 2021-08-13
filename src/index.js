799f1dff863b31039d1b8a6d8d46c0c4d11999b5
Iryna Lytvynenko <frau4145@gmail.com>
2021-08-13 11:02:29 +0300

    Add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (arguments.length === 0 || array.length === 0) return 0;
+    let min = array[0]
+    array.forEach(item => item < min ? min = item : null);
+    return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (arguments.length === 0 || array.length === 0) return 0;
+    let max = array[0]
+    array.forEach(item => item > max ? max = item : null);
+    return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (arguments.length === 0 || array.length === 0) return 0;
+    return array.reduce((acc, item) => acc + item) / array.length;
 }
